baadffc07306a97ef2f480011276ccf7363cc12f
kupermanak <kupermanak@gmail.com>
2022-02-09 23:29:02 +0000

    first update

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse_generator = document.getElementById("excuse");
+  function onLoad() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whoind = Math.floor(Math.random() * who.length);
+    let actionind = Math.floor(Math.random() * action.length);
+    let whatind = Math.floor(Math.random() * what.length);
+    let whenind = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoind] +
+      " " +
+      action[actionind] +
+      " " +
+      what[whatind] +
+      " " +
+      when[whenind]
+    );
+  }
+  excuse_generator.innerHTML = onLoad();
 };
